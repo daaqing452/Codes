e06b2d6706ccb36dd7b8dd9be8041b49a7e8d3d3
Yiqin Lu <daaqing452@gmail.com>
2018-09-19 17:13:09 +0800

    Add java class judger

---
+++ judger/judger.sh
@@ -0,0 +1,26 @@
+probs=("helloworld" "gogo")
+
+
+for file in user/*
+do
+	if [ -d "$file" ]
+	then
+		echo $file
+		cd $file/src/
+		for prob in ${probs[*]}
+		do
+			cp ../../../test/$prob/Test.java $prob/Test.java 2> tmp.txt
+			javac $prob/*.java 2> tmp.txt
+			java $prob.Test ../../../test/$prob/input.txt > output.txt 2> tmp.txt
+			diff output.txt ../../../test/$prob/output.txt > /dev/null
+			if [ $? != 0 ]
+			then
+				echo $prob "WA"
+			else
+				echo $prob "AC"
+			fi
+		done
+		echo ""
+		cd ../../../
+	fi
+done
